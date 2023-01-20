8cde79da6c892a454ba35cc389cf7796378c914d
Egor Karpushin <damiengo@rambler.ru>
2023-01-20 16:10:42 +1100

    Relocate folders

---
+++ annapashina/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-nb*hrnv(7sht^+aol=#$t!t4fpu68+493usg+5f92-_f(5h1l&"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
