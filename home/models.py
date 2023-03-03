6d84a33ec1fccc4cf17cf02948cb94f6a1c5a029
Egor Karpushin <damiengo@rambler.ru>
2023-03-03 16:13:23 +1100

    Правки верстки

--- home/models.py
+++ home/models.py
@@ -7,6 +7,26 @@ from wagtail.admin.edit_handlers import FieldPanel
 class HomePage(Page):
     subpage_types = ['NewPage']
     
+    # main_title = models.CharField(
+    #     max_length=200,
+    #     blank=True,
+    #     null=True,
+    #     verbose_name='Заглавный встречающий текст',
+    # )
+    # sub_title = models.CharField(
+    #     max_length=200,
+    #     blank=True,
+    #     null=True,
+    #     verbose_name='Дополнительный встречающий текст',
+    # )
+    # main_photo = models.ImageField(
+    #     upload_to = 'images/',
+    #     blank = True,
+    #     null = True,
+    #     verbose_name = 'Изображение для окончания турнира'
+    # )
+    
+    
 
 class NewPage(Page):    
     parent_page_types = ['HomePage']
